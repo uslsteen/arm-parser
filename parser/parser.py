daa18155c992f666217b01324fb9bed42a45bed6
uslsteen <shurygin0171@gmail.com>
2022-11-02 00:20:27 +0300

    [arm-parser] add instruction fields parsing

--- parser/parser.py
+++ parser/parser.py
@@ -12,7 +12,8 @@ from itertools import takewhile
 
 from matplotlib import widgets
 
-ALL_XML, XML_REGULAR = '*.xml', '.*/(\S+).xml'
+ALL_XML, XML_REGULAR = str('*.xml'),\
+                       str('.*/(\S+).xml')
 
 class Instruction:
     def __init__(self, name, encs, post, conditional, exec):
@@ -50,36 +51,49 @@ class ArmParser():
     #
     def read_data(self, xml):
         encs = list()
-        instr_data = dict()
 
         for iclass in xml.findall('.//classes/iclass'):
-            mask = str()
+            instr_data = dict()
+            enc_mask = str()
+            #
             docvars = iclass.find("docvars")
             for doc_var in docvars:
                 instr_data[doc_var.get("key")] = doc_var.get("value")
 
             encoding = iclass.find('regdiagram')
-            #
             ps_name = encoding.attrib.get('psname')
             fields = list()
             #
             for box in encoding.findall('box'):
                 field_name = box.attrib.get('name')
-                #s
-                if field_name != None:
-                    fields.append(field_name)
-                #
                 width, msb = int(box.attrib.get('width','1')), int(box.attrib['hibit'])
                 lsb = msb - width + 1
-
+                #
+                if field_name != None:
+                    fields.append(field_name)
+                    if self.instr_fields.get(field_name) == None:
+                        self.instr_fields[field_name] = make_fields_data(msb, lsb)
+                #
                 for b_it in box:
-                    mask += str(b_it.text) if b_it.text != None else "x" * width
+                    enc_mask += str(b_it.text) if b_it.text != None else "x" * width
             
-            instr_data["mask"] = mask
+            instr_data["mask"] = enc_mask
             instr_data["fields"] = fields
             instr_data["ps_name"] = ps_name
 
             self.insts_list.append(instr_data)
-
+            #
+    #
             
-            
+           
+def ones(n) -> int:
+    return (1 << n) - 1
+    #
+#
+def get_mask(from_, to_) -> int:
+    return ones(from_ - to_ + 1) << from_
+    #
+#
+def make_fields_data(msb, lsb) -> dict:
+    fld_mask = get_mask(msb, lsb)
+    return dict({"bits" : dict({"msb" : msb, "lsb" : lsb}), "mask" : fld_mask, "hex_mask" : hex(fld_mask)})
