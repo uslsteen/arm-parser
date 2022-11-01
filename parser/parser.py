f9e468b66beac08b24f2e09c5ba1451cd017b4ef
uslsteen <shurygin0171@gmail.com>
2022-11-02 01:50:25 +0300

    [arm-parser] fix instr type

--- parser/parser.py
+++ parser/parser.py
@@ -16,13 +16,18 @@ ALL_XML, XML_REGULAR = str('*.xml'),\
                        str('.*/(\S+).xml')
 
 class Instruction:
-    def __init__(self, name, encs, post, conditional, exec):
-        self.name = name
+    def __init__(self, mn : str = str(), mask : str = str(), isa : str = str(), ps_name : str = str(),\
+                       encs : list = list(), cond_setting : str = str(), instr_class : str = str()):
+        self.mnemonic = mn
+        self.mask = mask
+        self.isa = isa
+        self.ps_name = ps_name
         self.encs = encs
-        self.post = post
-        self.conditional = conditional
-        self.exec = exec
-
+        self.cond_setting = cond_setting
+        self.instr_class = instr_class
+        self.fields = list()
+        #
+    #
 
 class ArmParser():
     def __init__(self, path):
@@ -32,6 +37,8 @@ class ArmParser():
         self.instr_fields = dict()
         self.instructions = dict()
         self.insts_list = list()
+        #
+        self.not_impl_attr = set()
         #
     #
     def collect(self):
@@ -53,13 +60,19 @@ class ArmParser():
         encs = list()
 
         for iclass in xml.findall('.//classes/iclass'):
-            instr_data = dict()
+            instr_data = Instruction()
             enc_mask = str()
             #
             docvars = iclass.find("docvars")
             for doc_var in docvars:
-                instr_data[doc_var.get("key")] = doc_var.get("value")
-
+                attr = str(doc_var.get("key"))
+                attr = attr.replace("-", "_")
+                #
+                if hasattr(instr_data, attr):
+                    setattr(instr_data, attr, doc_var.get("value"))
+                else:
+                    self.not_impl_attr.add(attr)
+                    
             encoding = iclass.find('regdiagram')
             ps_name = encoding.attrib.get('psname')
             fields = list()
@@ -77,9 +90,10 @@ class ArmParser():
                 for b_it in box:
                     enc_mask += str(b_it.text) if b_it.text != None else "x" * width
             
-            instr_data["mask"] = enc_mask
-            instr_data["fields"] = fields
-            instr_data["ps_name"] = ps_name
+
+            instr_data.mask = enc_mask
+            instr_data.fields = fields
+            instr_data.ps_name = ps_name
 
             self.insts_list.append(instr_data)
             #
