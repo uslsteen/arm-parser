d83a123f8be9ee3e0a4fffdf2f641dbf7af246f2
uslsteen <shurygin0171@gmail.com>
2022-11-02 17:10:33 +0300

    [arm-parser] ArmParser refactoring

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 from parser.parser import ArmParser
+import argparse
 
 def main():
     #
-    parser = ArmParser(Path("/home/anton/code/arm-asl-parser/xml/ISA_v85A_A64_xml_00bet9"))
+    parser = argparse.ArgumentParser(description="ARM parser - tool to get instruction data from xml sources")
+
+    parser.add_argument('-dir', "--directory", type=str, help="Path to xml sources directory")
+    # args = parser.parse_args()
+
+    exec_path = Path.resolve(Path(__file__)).parent
+    # xml_path = Path(args.dir)
+    #
+    parser = ArmParser(exec_path.joinpath("xml", "ISA_v85A_A64_xml_00bet9"))
     parser.collect()
     parser.parse()
     #
--- parser/parser.py
+++ parser/parser.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 ALL_XML, XML_REGULAR = str('*.xml'),\
                        str('.*/(\S+).xml')
 
+# NOTE: 
 class Instruction:
     def __init__(self):
         self.mnemonic = str()
@@ -25,8 +26,9 @@ class Instruction:
         #
     #
 
+# NOTE: 
 class ArmParser():
-    def __init__(self, path):
+    def __init__(self, path : Path):
         self.path = path
         self.xml_list = list()
         #
@@ -56,52 +58,72 @@ class ArmParser():
         encs = list()
 
         for iclass in xml.findall('.//classes/iclass'):
-            instr_data = Instruction()
-            mask = str()
             #
-            docvars = iclass.find("docvars")
-            for doc_var in docvars:
-                attr = str(doc_var.get("key"))
-                attr = attr.replace("-", "_")
-                #
-                if hasattr(instr_data, attr):
-                    setattr(instr_data, attr, doc_var.get("value"))
-                else:
-                    self.not_impl_attr.add(attr)
-                    
-            encoding = iclass.find('regdiagram')
-            ps_name = encoding.attrib.get('psname')
-            fields = list()
-            #
-            for box in encoding.findall('box'):
-                field_name = box.attrib.get('name')
-                width, msb = int(box.attrib.get('width','1')), int(box.attrib['hibit'])
-                lsb = msb - width + 1
-                #
-                if field_name != None:
-                    fields.append(field_name)
-                    if self.instr_fields.get(field_name) == None:
-                        self.instr_fields[field_name] = make_fields_data(msb, lsb)
-                #
-                for b_it in box:
-                    mask += set_bits(b_it.text, width)
-            
-            instr_data.mask = mask
-            instr_data.fields = fields
-            instr_data.ps_name = ps_name
+            instr_data = self.parse_instr_section(iclass)
 
-            self.insts_list.append(instr_data)
+            encoding = iclass.find('regdiagram')
+
+            instr_data.ps_name = deslash(encoding.attrib.get('psname'))
+            instr_data.mask, instr_data.fields = self.parse_bits_section(encoding)
+
+            if instr_data.mnemonic != str():
+                self.insts_list.append(instr_data)
+            else:
+                print(instr_data.ps_name)
+            
+            encs.append(instr_data)
             #
     #
+    def parse_instr_section(self, iclass) -> Instruction:
+    #
+        instr_data = Instruction()
+
+        docvars = iclass.find("docvars")
+        for doc_var in docvars:
+            attr = str(doc_var.get("key"))
+            attr = attr.replace("-", "_")
+            #
+            if hasattr(instr_data, attr):
+                setattr(instr_data, attr, doc_var.get("value"))
+            else:
+                self.not_impl_attr.add(attr)
+            #
+        return instr_data   
+    #
+    def parse_bits_section(self, encoding):
+    #   
+        fields, mask = list(), str()
+
+        for box in encoding.findall('box'):
+            field_name = box.attrib.get('name')
+            width, msb = int(box.attrib.get('width','1')), int(box.attrib['hibit'])
+            lsb = msb - width + 1
+            #
+            if field_name != None:
+                fields.append(field_name)
+                if self.instr_fields.get(field_name) == None:
+                    self.instr_fields[field_name] = make_fields_data(msb, lsb)
+            #
+            for b_it in box:
+                mask += set_bits(b_it.text, width)
+        
+        return mask, fields
+
+
 def set_bits(bit : str, width) -> bool:
+    #
     if bit in ['1', '0']:
         return bit
-    elif bit in ['(1)', '(0)', 'x']:
+    elif bit == 'x':
         return 'x'
+    elif bit == '(1)':
+        return '1'
+    elif bit == '(0)':
+        return '0'
     else:
         return 'x' * width  
-        #
-    #      
+    #
+#      
 def ones(n) -> int:
     return (1 << n) - 1
     #
@@ -113,3 +135,7 @@ def get_mask(from_, to_) -> int:
 def make_fields_data(msb, lsb) -> dict:
     fld_mask = get_mask(msb, lsb)
     return dict({"bits" : dict({"msb" : msb, "lsb" : lsb}), "mask" : fld_mask, "hex_mask" : hex(fld_mask)})
+    #
+#
+def deslash(name : str) -> str:
+    return name.replace("/instrs","").replace("/", "_").replace("-","_")
